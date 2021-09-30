bfcaf6aec4f775e49e29c91927cc3d8fdfa1067b
quovanitish <nitish.sharma1@quovantis.com>
2021-09-30 18:09:18 +0530

    Function to print the board

--- Assignments/Day 3/Tic Tac Toe Class/index.js
+++ Assignments/Day 3/Tic Tac Toe Class/index.js
@@ -13,6 +13,22 @@ class TicTacToe {
     this.playerWon = undefined;
     this.totalMoves = 0;
   }
+
+  // function to print the board
+  printBoard() {
+    console.log();
+    console.log("-------------");
+    for (let i = 0; i < this.board.length; i++) {
+      let line = "";
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (j == 0) line += "| " + this.board[i][j] + " | ";
+        else line += this.board[i][j] + " | ";
+      }
+      console.log(line);
+      console.log("-------------");
+    }
+    console.log("");
+  }
 }
 
 let board = [['X', ' ', '0'], [' ', 'X', ' '], ['0', ' ', ' ']]

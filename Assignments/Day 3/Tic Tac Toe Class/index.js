33b58fc6a241f6818fdb24452586a20ed0a2fddc
quovanitish <nitish.sharma1@quovantis.com>
2021-09-30 18:10:22 +0530

    Function to check winner and check a move performed valid or not

--- Assignments/Day 3/Tic Tac Toe Class/index.js
+++ Assignments/Day 3/Tic Tac Toe Class/index.js
@@ -29,6 +29,92 @@ class TicTacToe {
     }
     console.log("");
   }
+
+  // function to check the winner in the game
+  checkWinner() {
+    if (
+      this.board[0][0] == this.board[0][1] &&
+      this.board[0][0] == this.board[0][2] &&
+      this.board[0][0] != " "
+    ) {
+      this.playerWon = this.board[0][0];
+    }
+  
+    // 2. Combination 2 horizontal second row
+    else if (
+      this.board[1][0] == this.board[1][1] &&
+      this.board[1][0] == this.board[1][2] &&
+      this.board[1][0] != " "
+    ) {
+      this.playerWon = this.board[1][0];
+    }
+  
+    // 3. Combination 3 horizontal third row
+    else if (
+      this.board[2][0] == this.board[2][1] &&
+      this.board[2][0] == this.board[2][2] &&
+      this.board[2][0] != " "
+    ) {
+      this.playerWon = this.board[2][0];
+    }
+  
+    // 4. Combination 4 Diagonal 1
+    else if (
+      this.board[0][0] == this.board[1][1] &&
+      this.board[0][0] == this.board[2][2] &&
+      this.board[0][0] != " "
+    ) {
+      this.playerWon = this.board[0][0];
+    }
+  
+    // 5. Combination 5 Diagonal 2
+    else if (
+      this.board[2][0] == this.board[1][1] &&
+      this.board[2][0] == this.board[0][2] &&
+      this.board[2][0] != " "
+    ) {
+      this.playerWon = this.board[2][0];
+    }
+  
+    // 6. Combination 6 vertical first column
+    else if (
+      this.board[0][0] == this.board[1][0] &&
+      this.board[0][0] == this.board[2][0] &&
+      this.board[0][0] != " "
+    ) {
+      this.playerWon = this.board[0][0];
+    }
+  
+    // 7. Combination 7 vertical second column
+    else if (
+      this.board[0][1] == this.board[1][1] &&
+      this.board[0][1] == this.board[2][1] &&
+      this.board[0][1] != " "
+    ) {
+      this.playerWon = this.board[0][1];
+    }
+  
+    // 8. Combination 8 vertical third column
+    else if (
+      this.board[0][2] == this.board[1][2] &&
+      this.board[0][2] == this.board[2][2] &&
+      this.board[0][2] != " "
+    ) {
+      this.playerWon = this.board[0][2];
+    } else if (this.playerWon == undefined && this.totalMoves == 9) {
+      console.log("Game Tie !!".blue.bold);
+      process.exit();
+    }
+    return;
+  };
+
+  // function to check a performed move is valid or not
+  checkValidMove(rowNum, colNum) {
+    if(this.board[rowNum][colNum] != ' ' || this.board[rowNum][colNum] == undefined) 
+        return false;
+    else
+        return true;
+  }
 }
 
 let board = [['X', ' ', '0'], [' ', 'X', ' '], ['0', ' ', ' ']]
